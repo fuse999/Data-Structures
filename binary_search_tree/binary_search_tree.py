fb40235d453bd60848106b165ea5769616ca0089
riley pence <rileypence4@gmail.com>
2020-03-25 13:46:04 -0700

    adding contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -13,20 +13,44 @@ class BinarySearchTree:
     # Insert the given value into the tree
     def insert(self, value):
         if value < self.value:# if arg less than current root
+
             if not self.left:# if left of root is empty
                 self.left = BinarySearchTree(value)# insert left of root
+
             else:# if there left value
                 self.left.insert(value)# set left as new root for insert
+
         else:# if arg more than current root
+
             if not self.right:# if right of root is empty
                 self.right = BinarySearchTree(value)# insert right of root
+
             else:# if there right value
                 self.right.insert(value)# set right as new root for insert
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:# if root is target
+            return True
+
+        if target < self.value:# if target less than root
+
+            if not self.left:# if ther no left return false
+                return False
+
+            else:# if there is a left
+                # set left as new root for contains
+                return self.left.contains(target)
+
+        else:# if target more than root
+            
+            if not self.right:# if ther no right return false
+                return False
+
+            else:# if there is a right
+                # set right as new root for contains
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
